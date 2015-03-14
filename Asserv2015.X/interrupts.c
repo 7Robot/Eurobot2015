d3277bb5d31a7d123d51e2a9e9251547587502c9
AlexandreN7 <prxo@live.fr>
2015-03-14 18:03:38 +0100

    ajout du fichiers d'interruption ajout d'un dossier de doc

---
+++ Asserv2015.X/interrupts.c
@@ -0,0 +1,270 @@
+#include <xc.h>
+
+#include <stdint.h>        /* Includes uint16_t definition   */
+#include <stdbool.h>       /* Includes true/false definition */
+#include <timer.h>
+#include <uart.h>
+
+#include "extern_globals.h"
+#include "user.h"
+#include <libpic30.h>
+#include "tests.h"
+#include "lib_asserv/lib_asserv.h"
+#include "motor.h"
+#include "ax12.h"
+#include "atp-asserv.h"
+#include "actions_ax12.h"
+
+void InitTimers()
+{
+    _ODCB5 = 1; // Open drain sur la pin RB5(pour les AX12)
+    _NSTDIS = 0; // activation de la priorité des interruptions
+    AD1PCFGL = 0xFFFF; //Pins analogiques en numérique pour que ATP marche !!
+
+
+    OpenUART2(UART_EN & UART_IDLE_CON & UART_IrDA_DISABLE & UART_MODE_FLOW
+        & UART_UEN_00 & UART_DIS_WAKE & UART_DIS_LOOPBACK
+        & UART_DIS_ABAUD & UART_UXRX_IDLE_ONE & UART_BRGH_SIXTEEN
+        & UART_NO_PAR_8BIT & UART_1STOPBIT,
+          UART_INT_TX_BUF_EMPTY & UART_IrDA_POL_INV_ZERO
+        & UART_SYNC_BREAK_DISABLED & UART_TX_ENABLE & UART_TX_BUF_NOT_FUL & UART_INT_RX_CHAR
+        & UART_ADR_DETECT_DIS & UART_RX_OVERRUN_CLEAR,
+          BRGVALAX12);
+
+    ConfigIntUART2(UART_RX_INT_PR5 & UART_RX_INT_EN
+                 & UART_TX_INT_PR5 & UART_TX_INT_DIS);
+    
+    // activation du timer 2
+    OpenTimer2(T2_ON &
+                T2_IDLE_CON &
+                T2_GATE_OFF &
+                T2_PS_1_64 &
+                T2_SOURCE_INT, 3125 ); // 3125 pour 5ms
+    // configuration des interruptions
+    ConfigIntTimer2(T2_INT_PRIOR_4 & T2_INT_ON);
+    
+    // Ici interruption des actions des bras
+    IFS2bits.SPI2IF = 0; // Flag SPI2 Event Interrupt Priority
+    IPC8bits.SPI2IP = 2; // Priority SPI2 Event Interrupt Priority
+    IEC2bits.SPI2IE = 1; //Enable SPI2 Event Interrupt Priority
+
+
+    
+
+}
+
+void Init_CN()
+{
+    _TRISA9 = 1;  // input for button
+    _TRISC3 = 1;  // input for laisse
+    _CN28IE = 1; // Enable CN28 pin for interrupt detection
+    IPC4bits.CNIP = 3; //Interrupt level 3
+    IEC1bits.CNIE = 1; // Enable CN interrupts
+    IFS1bits.CNIF = 0; // Reset CN interrupt
+}
+
+/******************************************************************************/
+/* Interrupt Vector Options                                                   */
+/******************************************************************************/
+/*                                                                            */
+/* Refer to the C30 (MPLAB C Compiler for PIC24F MCUs and dsPIC33F DSCs) User */
+/* Guide for an up to date list of the available interrupt options.           */
+/* Alternately these names can be pulled from the device linker scripts.      */
+/*                                                                            */
+/* dsPIC33F Primary Interrupt Vector Names:                                   */
+/*                                                                            */
+/* _INT0Interrupt      _C1Interrupt                                           */
+/* _IC1Interrupt       _DMA3Interrupt                                         */
+/* _OC1Interrupt       _IC3Interrupt                                          */
+/* _T1Interrupt        _IC4Interrupt                                          */
+/* _DMA0Interrupt      _IC5Interrupt                                          */
+/* _IC2Interrupt       _IC6Interrupt                                          */
+/* _OC2Interrupt       _OC5Interrupt                                          */
+/* _T2Interrupt        _OC6Interrupt                                          */
+/* _T3Interrupt        _OC7Interrupt                                          */
+/* _SPI1ErrInterrupt   _OC8Interrupt                                          */
+/* _SPI1Interrupt      _DMA4Interrupt                                         */
+/* _U1RXInterrupt      _T6Interrupt                                           */
+/* _U1TXInterrupt      _T7Interrupt                                           */
+/* _ADC1Interrupt      _SI2C2Interrupt                                        */
+/* _DMA1Interrupt      _MI2C2Interrupt                                        */
+/* _SI2C1Interrupt     _T8Interrupt                                           */
+/* _MI2C1Interrupt     _T9Interrupt                                           */
+/* _CNInterrupt        _INT3Interrupt                                         */
+/* _INT1Interrupt      _INT4Interrupt                                         */
+/* _ADC2Interrupt      _C2RxRdyInterrupt                                      */
+/* _DMA2Interrupt      _C2Interrupt                                           */
+/* _OC3Interrupt       _DCIErrInterrupt                                       */
+/* _OC4Interrupt       _DCIInterrupt                                          */
+/* _T4Interrupt        _DMA5Interrupt                                         */
+/* _T5Interrupt        _U1ErrInterrupt                                        */
+/* _INT2Interrupt      _U2ErrInterrupt                                        */
+/* _U2RXInterrupt      _DMA6Interrupt                                         */
+/* _U2TXInterrupt      _DMA7Interrupt                                         */
+/* _SPI2ErrInterrupt   _C1TxReqInterrupt                                      */
+/* _SPI2Interrupt      _C2TxReqInterrupt                                      */
+/* _C1RxRdyInterrupt                                                          */
+/*                                                                            */
+/* dsPIC33E Primary Interrupt Vector Names:                                   */
+/*                                                                            */
+/* _INT0Interrupt     _IC4Interrupt      _U4TXInterrupt                       */
+/* _IC1Interrupt      _IC5Interrupt      _SPI3ErrInterrupt                    */
+/* _OC1Interrupt      _IC6Interrupt      _SPI3Interrupt                       */
+/* _T1Interrupt       _OC5Interrupt      _OC9Interrupt                        */
+/* _DMA0Interrupt     _OC6Interrupt      _IC9Interrupt                        */
+/* _IC2Interrupt      _OC7Interrupt      _PWM1Interrupt                       */
+/* _OC2Interrupt      _OC8Interrupt      _PWM2Interrupt                       */
+/* _T2Interrupt       _PMPInterrupt      _PWM3Interrupt                       */
+/* _T3Interrupt       _DMA4Interrupt     _PWM4Interrupt                       */
+/* _SPI1ErrInterrupt  _T6Interrupt       _PWM5Interrupt                       */
+/* _SPI1Interrupt     _T7Interrupt       _PWM6Interrupt                       */
+/* _U1RXInterrupt     _SI2C2Interrupt    _PWM7Interrupt                       */
+/* _U1TXInterrupt     _MI2C2Interrupt    _DMA8Interrupt                       */
+/* _AD1Interrupt      _T8Interrupt       _DMA9Interrupt                       */
+/* _DMA1Interrupt     _T9Interrupt       _DMA10Interrupt                      */
+/* _NVMInterrupt      _INT3Interrupt     _DMA11Interrupt                      */
+/* _SI2C1Interrupt    _INT4Interrupt     _SPI4ErrInterrupt                    */
+/* _MI2C1Interrupt    _C2RxRdyInterrupt  _SPI4Interrupt                       */
+/* _CM1Interrupt      _C2Interrupt       _OC10Interrupt                       */
+/* _CNInterrupt       _QEI1Interrupt     _IC10Interrupt                       */
+/* _INT1Interrupt     _DCIEInterrupt     _OC11Interrupt                       */
+/* _AD2Interrupt      _DCIInterrupt      _IC11Interrupt                       */
+/* _IC7Interrupt      _DMA5Interrupt     _OC12Interrupt                       */
+/* _IC8Interrupt      _RTCCInterrupt     _IC12Interrupt                       */
+/* _DMA2Interrupt     _U1ErrInterrupt    _DMA12Interrupt                      */
+/* _OC3Interrupt      _U2ErrInterrupt    _DMA13Interrupt                      */
+/* _OC4Interrupt      _CRCInterrupt      _DMA14Interrupt                      */
+/* _T4Interrupt       _DMA6Interrupt     _OC13Interrupt                       */
+/* _T5Interrupt       _DMA7Interrupt     _IC13Interrupt                       */
+/* _INT2Interrupt     _C1TxReqInterrupt  _OC14Interrupt                       */
+/* _U2RXInterrupt     _C2TxReqInterrupt  _IC14Interrupt                       */
+/* _U2TXInterrupt     _QEI2Interrupt     _OC15Interrupt                       */
+/* _SPI2ErrInterrupt  _U3ErrInterrupt    _IC15Interrupt                       */
+/* _SPI2Interrupt     _U3RXInterrupt     _OC16Interrupt                       */
+/* _C1RxRdyInterrupt  _U3TXInterrupt     _IC16Interrupt                       */
+/* _C1Interrupt       _USB1Interrupt     _ICDInterrupt                        */
+/* _DMA3Interrupt     _U4ErrInterrupt    _PWMSpEventMatchInterrupt            */
+/* _IC3Interrupt      _U4RXInterrupt     _PWMSecSpEventMatchInterrupt         */
+/*                                                                            */
+/* For alternate interrupt vector naming, simply add 'Alt' between the prim.  */
+/* interrupt vector name '_' and the first character of the primary interrupt */
+/* vector name.  There is no Alternate Vector or 'AIVT' for the 33E family.   */
+/*                                                                            */
+/* For example, the vector name _ADC2Interrupt becomes _AltADC2Interrupt in   */
+/* the alternate vector table.                                                */
+/*                                                                            */
+/* Example Syntax:                                                            */
+/*                                                                            */
+/* void __attribute__((interrupt,auto_psv)) <Vector Name>(void)               */
+/* {                                                                          */
+/*     <Clear Interrupt Flag>                                                 */
+/* }                                                                          */
+/*                                                                            */
+/* For more comprehensive interrupt examples refer to the C30 (MPLAB C        */
+/* Compiler for PIC24 MCUs and dsPIC DSCs) User Guide in the                  */
+/* <C30 compiler instal directory>/doc directory for the latest compiler      */
+/* release.  For XC16, refer to the MPLAB XC16 C Compiler User's Guide in the */
+/* <XC16 compiler instal directory>/doc folder.                               */
+/*                                                                            */
+/******************************************************************************/
+/* Interrupt Routines                                                         */
+/******************************************************************************/
+
+/* TODO Add interrupt routine code here. */
+
+void __attribute__((interrupt,auto_psv)) _T2Interrupt(void) {
+    // on baisse le flag
+    _T2IF = 0;
+    // compteurs QEI gauche et droit
+    volatile static int tics_g, tics_d;
+    // commandes gauches et droite
+    volatile static float commande_g, commande_d;
+
+    // récupération des données des compteurs qei gauche et droit
+    tics_g = (int)POS1CNT;
+    tics_d = (int)POS2CNT;
+    // effectuer un pas de déplacement
+    motion_step(tics_g,tics_d, &commande_g, &commande_d);
+    // mettre ici les pwm gauche et droit
+    PWM_Moteurs(commande_g, commande_d);
+}
+
+/*************************************************
+* TX et RX Interrupt *
+*************************************************/
+void __attribute__((interrupt, no_auto_psv)) _U2RXInterrupt(void){
+    _U2RXIF = 0; // On baisse le FLAG
+    led = !led;
+    InterruptAX();
+}
+
+void __attribute__((__interrupt__, no_auto_psv)) _U2TXInterrupt(void){
+   _U2TXIF = 0; // clear TX interrupt flag
+}
+
+
+// interruption lié au bras du robot
+void __attribute__((interrupt, no_auto_psv)) _SPI2Interrupt(void){
+    led=1;
+    IFS2bits.SPI2IF = 0;
+//    if ((actionBras & BOUGIE_init_arm) == BOUGIE_init_arm) {
+//        init_arm(numBras);
+//        actionBras &= ~BOUGIE_init_arm;
+//
+//    } else if ((actionBras & BOUGIE_catch_arm) == BOUGIE_catch_arm) {
+//       catch_arm( numBras);
+//        actionBras &= ~BOUGIE_catch_arm;
+//
+//    } else if ((actionBras & BOUGIE_stock_arm) == BOUGIE_stock_arm) {
+//        stock_arm(numBras);
+//        actionBras &= ~BOUGIE_stock_arm;
+//
+//    } else if ((actionBras & BOUGIE_pull_arm) == BOUGIE_pull_arm) {
+//        pull_arm( numBras);
+//        actionBras &= ~BOUGIE_pull_arm;
+//
+//    } else if ((actionBras & BOUGIE_push_arm) == BOUGIE_push_arm) {
+//        push_arm( numBras);
+//        actionBras &= ~BOUGIE_push_arm;
+//
+//    } else if ((actionBras & BOUGIE_launch_net) == BOUGIE_launch_net) {
+//        launch_net();
+//        actionBras &= ~BOUGIE_launch_net;
+//
+//    } else if ((actionBras & BOUGIE_convoyer) == BOUGIE_convoyer) {
+//        convoyer();
+//        actionBras &= ~BOUGIE_convoyer;
+//
+//    } else if ((actionBras & BOUGIE_slight_convoyer) == BOUGIE_slight_convoyer) {
+//        slight_convoyer();
+//        actionBras &= ~BOUGIE_slight_convoyer;
+//
+//    } else if ((actionBras & BOUGIE_raise_arm) == BOUGIE_raise_arm ) {
+//        raise_arm( numBras);
+//        actionBras &= ~BOUGIE_raise_arm;
+//
+//    } else if ((actionBras & BOUGIE_salut) == BOUGIE_salut ) {
+//        salut( numBras);
+//        actionBras &= ~BOUGIE_salut;
+//    }  else if ((actionBras & BOUGIE_poulet) == BOUGIE_poulet ) {
+//        poulet();
+//        actionBras &= ~BOUGIE_poulet;
+//    }
+    
+}
+
+/**********************************************/
+/* CN interrupt for boutons */
+
+void __attribute__ ((__interrupt__, no_auto_psv)) _CNInterrupt(void)
+{
+    if (!PIN_LAISSE)
+    {
+        SendStart(BOUTON_COULEUR);
+    }
+    else
+    {
+        __delay_ms(500);
+    }
+    IFS1bits.CNIF = 0; // Clear CN interrupt
+}
--- Asserv2015.X/nbproject/Makefile-default.mk
+++ Asserv2015.X/nbproject/Makefile-default.mk
@@ -45,17 +45,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=main.c motor.c user.c lib_asserv/private/tools.c
+SOURCEFILES_QUOTED_IF_SPACED=main.c motor.c user.c lib_asserv/private/tools.c interrupts.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.o ${OBJECTDIR}/motor.o ${OBJECTDIR}/user.o ${OBJECTDIR}/lib_asserv/private/tools.o
-POSSIBLE_DEPFILES=${OBJECTDIR}/main.o.d ${OBJECTDIR}/motor.o.d ${OBJECTDIR}/user.o.d ${OBJECTDIR}/lib_asserv/private/tools.o.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.o ${OBJECTDIR}/motor.o ${OBJECTDIR}/user.o ${OBJECTDIR}/lib_asserv/private/tools.o ${OBJECTDIR}/interrupts.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/main.o.d ${OBJECTDIR}/motor.o.d ${OBJECTDIR}/user.o.d ${OBJECTDIR}/lib_asserv/private/tools.o.d ${OBJECTDIR}/interrupts.o.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/main.o ${OBJECTDIR}/motor.o ${OBJECTDIR}/user.o ${OBJECTDIR}/lib_asserv/private/tools.o
+OBJECTFILES=${OBJECTDIR}/main.o ${OBJECTDIR}/motor.o ${OBJECTDIR}/user.o ${OBJECTDIR}/lib_asserv/private/tools.o ${OBJECTDIR}/interrupts.o
 
 # Source Files
-SOURCEFILES=main.c motor.c user.c lib_asserv/private/tools.c
+SOURCEFILES=main.c motor.c user.c lib_asserv/private/tools.c interrupts.c
 
 
 CFLAGS=
@@ -110,6 +110,13 @@ ${OBJECTDIR}/lib_asserv/private/tools.o: lib_asserv/private/tools.c  nbproject/M
 	${MP_CC} $(MP_EXTRA_CC_PRE)  lib_asserv/private/tools.c  -o ${OBJECTDIR}/lib_asserv/private/tools.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/lib_asserv/private/tools.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1    -omf=elf -O0 -msmart-io=1 -Wall -msfr-warn=off
 	@${FIXDEPS} "${OBJECTDIR}/lib_asserv/private/tools.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
 	
+${OBJECTDIR}/interrupts.o: interrupts.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/interrupts.o.d 
+	@${RM} ${OBJECTDIR}/interrupts.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  interrupts.c  -o ${OBJECTDIR}/interrupts.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/interrupts.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1    -omf=elf -O0 -msmart-io=1 -Wall -msfr-warn=off
+	@${FIXDEPS} "${OBJECTDIR}/interrupts.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
+	
 else
 ${OBJECTDIR}/main.o: main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
@@ -138,6 +145,13 @@ ${OBJECTDIR}/lib_asserv/private/tools.o: lib_asserv/private/tools.c  nbproject/M
 	@${RM} ${OBJECTDIR}/lib_asserv/private/tools.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  lib_asserv/private/tools.c  -o ${OBJECTDIR}/lib_asserv/private/tools.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/lib_asserv/private/tools.o.d"        -g -omf=elf -O0 -msmart-io=1 -Wall -msfr-warn=off
 	@${FIXDEPS} "${OBJECTDIR}/lib_asserv/private/tools.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
+	
+${OBJECTDIR}/interrupts.o: interrupts.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/interrupts.o.d 
+	@${RM} ${OBJECTDIR}/interrupts.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  interrupts.c  -o ${OBJECTDIR}/interrupts.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/interrupts.o.d"        -g -omf=elf -O0 -msmart-io=1 -Wall -msfr-warn=off
+	@${FIXDEPS} "${OBJECTDIR}/interrupts.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
 	
 endif
 
