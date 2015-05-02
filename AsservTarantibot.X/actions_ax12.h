5aad6afd70c3172a212041835f38e1202157c615
RobinN7 <robin.beilvert@gmail.com>
2015-05-02 22:46:43 +0200

    action ax12 tarantibot

--- AsservTarantibot.X/actions_ax12.h
+++ AsservTarantibot.X/actions_ax12.h
@@ -10,10 +10,13 @@
 
 
 
-#define rab           7  // le rabateur
-#define pince          9  // la pince
-#define asc           11 // l'ascenceur
-#define tub            1    // le reservoir
+#define socle           42  // La fixation du bras droit sur le robot
+#define haut            12  // l'Ax-12 suivant : en haut du "levier"
+#define verseur_d       6   // L'actionneur de la plaque suppérieur droite
+#define verseur_g       16  // L'actionneur de la plaque suppérieur gauche
+#define pince_d         2   // L'actionneur de la plaque inférieur droite
+#define pince_g         10  // L'actionneur de la plaque inférieur gauche
+#define coude           13  // L'ax-12 permettant de ranger le bra gauche
 
 void choose_arm(int);
 void init_ax12(void);
